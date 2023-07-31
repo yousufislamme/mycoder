33bd81011276097ee92201b66c3dba4004d4f9e6
yousufislamme <online.okk@gmail.com>
2023-07-31 15:48:47 +0600

    edit text style

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h2>Hello Saadaf & Yousuf</h2>
+      <h1 className='text-5xl text-center font-extrabold text-purple-500 mt-11'>Hello Saadaf & Yousuf</h1>
     </main>
   )
 }
