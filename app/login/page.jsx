39987990db8a2a7ebe1c5b2c84ccae52ba2158a6
yousufislamme <online.okk@gmail.com>
2023-08-26 00:45:07 +0600

    Login with GitHub

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,54 +1,89 @@
-'use client'
-
-import Image from 'next/image';
-import React, { useState } from 'react'
-import {GoogleAuthProvider, getAuth, signInWithPopup, signOut} from 'firebase/auth'
-import app from '../firebase/Firebase.init';
+"use client";
  
-const Login = () => {
+import React, { useState } from "react";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../firebase/Firebase.init";
 
+const Login = () => {
   const [user, setUser] = useState(null);
   const auth = getAuth(app);
-  const provider = new GoogleAuthProvider();
-  const handleMyGoogleSing = () =>{
-    signInWithPopup(auth, provider)
-    .then(result => {
-      const userLogged =result.user;
-      console.log(userLogged);
-      setUser(userLogged);
-    })
+  const GoogleProvider = new GoogleAuthProvider();
+  const GithubProvider = new GithubAuthProvider();
 
-    .catch(error=>{
-      console.log('error', error.message);
-    })
-  }
+  const handleMyGoogleSing = () => {
+    signInWithPopup(auth, GoogleProvider)
+      .then((result) => {
+        const userLogged = result.user;
+        console.log(userLogged);
+        setUser(userLogged);
+      })
 
-  const handleSingOut = () => {
-    signOut(auth)
-    .then(result =>{
-      console.log(result);
-      setUser(null);
+      .catch((error) => {
+        console.log("error", error.message);
+      });
+  };
+
+  const handleGithubLogin = () => {
+    signInWithPopup(auth, GithubProvider)
+    .then(result=>{
+      const userLoggedGit = result.user;
+      console.log(userLoggedGit);
+      setUser(userLoggedGit);
     })
     .catch(error=>{
       console.log(error);
     })
   }
+
+  const handleSingOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  
   return (
-    <div className='h-screen'> 
-
-      <button className='border-2 border-blue-500 rounded-lg px-5 py-2' onClick={handleMyGoogleSing}
-      >Google Login</button>
-
-      <button onClick={handleSingOut} className='ml-5 border-2 border-red-500 rounded-lg px-5 py-2'>Sing Out</button>
-
-      {user && <div>
-        <h3>User Name: {user.displayName}</h3>
-        <p>Email: {user.email}</p>
-        <img src={user.photoURL} alt={user.displayName} />
-      </div>}
-
-    </div>
-  )
+    <div className="h-screen">
+      {user ?<button
+        onClick={handleSingOut}
+        className="ml-5 border-2 border-red-500 rounded-lg px-5 py-2"
+      >Sing Out
+      </button> 
+      :
+      <>
+      <button
+          className="border-2 border-blue-500 rounded-lg px-5 py-2"
+          onClick={handleMyGoogleSing}
+        >
+          Google Login
+      </button>
+      <button 
+       className="border-2 border-black rounded-lg px-5 py-2"
+      onClick={handleGithubLogin}>GitHub Login</button>
+      </>
+      
 }
+{/* Show on display */}
+      {user && (
+        <div>
+          <h3>User Name: {user.displayName}</h3>
+          <p>Email: {user.email}</p>
+          <img src={user.photoURL} alt={user.displayName} />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Login;
